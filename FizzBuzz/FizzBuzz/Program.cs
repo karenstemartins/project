23539a02b56aaf3dfb329f2d0e78d59198fbea56
obct537 <obct537@gmail.com>
2014-10-23 18:33:03 -0500

    Commit by Sam

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz ");
+                }
+                else
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
