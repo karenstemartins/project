24463aafaaefdfdb0d0191b68d328b2786fe9c37
karenstemartins <martik11@uwosh.edu>
2014-10-23 18:49:56 -0500

    I changed it!

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -13,21 +13,14 @@ namespace fizzBuzz
             for (int i = 0; i < 100; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
-                {
-                    System.Console.WriteLine("FizzBuzz ");
-                }
+                   System.Console.WriteLine("FizzBuzz ");
                 else if (i % 3 == 0)
-                {
-                    System.Console.WriteLine("Fizz ");
-                }
+                   System.Console.WriteLine("Fizz ");
                 else if (i % 5 == 0)
-                {
-                    System.Console.WriteLine("Buzz ");
-                }
+                   System.Console.WriteLine("Buzz ");
                 else
-                {
-                    System.Console.WriteLine(i);
-                }
+                   System.Console.WriteLine(i);
+                
             }
         }
     }
